dcc8e6653908da1630dd9267a6d5de68be667a44
Jorge Luis <jorgefloriano1646@gmail.com>
2025-07-17 16:49:48 -0300

    Interface atualizations

--- scripts.js
+++ scripts.js
@@ -1,3 +1,7 @@
 function enterInCourse() {
     window.open("https://www.youtube.com/watch?v=S9uPNppGsGo&list=PLHz_AreHm4dlKP6QQCekuIPky1CiwmdI6");
 }
+
+function showConsole() {
+    console.log("Hello World");
+}
